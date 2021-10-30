e6e0e30dbb5d333daac02e9051954e76bfaa0a3e
AndreasErslev <andreas.erslev@gmail.com>
2021-10-30 13:49:24 +0200

    Update to version 2

---
+++ Program/Version_one/Game/Attacks/Character/Slash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Roles.Enemies;
+
+namespace Game.Attacks.Character
+{
+    public class Slash : ICharacterAttack
+    {
+        public double Atack(IEnemy targetEnemy)
+        {
+            targetEnemy.TakeDamage(3);
+            return 0;
+        }
+    }
+}
---
+++ Program/Version_one/Game/Roles/Enemies/IEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Roles.Enemies
+{
+    public interface IEnemy
+    {
+        public void DealDamage()
+        {
+
+        }
+        public void Defend()
+        {
+
+        }
+
+        public void Heal()
+        {
+
+        }
+
+        public void CheckLevel()
+        {
+
+        }
+
+        public void TakeDamage(double damage)
+        {
+
+        }
+    }
+}
